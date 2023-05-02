4abf053b07648ac824eb7414840c191384f04366
Piotr K <piotr.keska67@gmail.com>
2023-05-02 21:19:29 +0200

    Refactor simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,8 +1,8 @@
 
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
-
-import APIClient ,{ FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
 const apiClient = new APIClient<Game>('/games');
@@ -33,7 +33,7 @@ const useGames = (gameQuery: GameQuery) =>
     getNextPageParam:(lastPage, allPages) => {
       return lastPage.next ? allPages.length + 1 : undefined;
     },
-    staleTime: 24 * 60 * 60 * 1000 //24h
+    staleTime: ms('24h') //24h
   })
 
 
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import genres from "../data/genres";
+import ms from 'ms';
 import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
 
 
 
@@ -20,7 +19,7 @@ export interface Genre {
 const useGenres = () => useQuery({
     queryKey: ['genres'],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24h
+    staleTime: ms('24h'), //24h
     // initialData: {count: genres.length, results: genres, next: null},
 })
 
