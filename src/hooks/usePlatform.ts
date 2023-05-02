2362efb37345f6438d0e938a0af0694eef04c8f0
Piotr K <piotr.keska67@gmail.com>
2023-05-02 21:11:51 +0200

    Create lookup hooks

---
+++ src/hooks/usePlatform.ts
@@ -0,0 +1,15 @@
+import usePlatforms from "./usePlatforms";
+
+
+const usePlatform = (id?: number) => {
+
+    const { data: platforms } = usePlatforms();
+    return platforms?.results.find(
+      (p) => p.id === id
+    );
+}
+    
+
+
+
+export default usePlatform;
